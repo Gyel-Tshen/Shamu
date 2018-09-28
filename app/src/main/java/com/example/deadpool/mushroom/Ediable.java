9755393de2636d78544ee52fb5a084fb0f44aa7f
Gyel-Tshen <tshegyel7777@gmail.com>
2018-09-28 23:32:46 +0600

    initukh

--- app/src/main/java/com/example/deadpool/mushroom/Ediable.java
+++ app/src/main/java/com/example/deadpool/mushroom/Ediable.java
@@ -1,9 +1,8 @@
 package com.example.deadpool.mushroom;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,14 +11,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Ediable extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    ListView listView;
+    ListViewAdapter adapter;
+    String[] title;
+    String[] description;
+    int[] icon;
+    ArrayList<Model> arrayList = new ArrayList<Model>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ediable);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -31,6 +41,26 @@ public class Ediable extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //listView
+        title = new String[]{"Agaricus", "Chanterelle", "Crimini", "Shiitake", "Oyster", "Enoki", "Portabello", "Porcini", "Morel"};
+        description = new String[]{"Agaricus detail...", "Chanterelle detail...", "Crimini detail...", "Shiitake detail...", "Oyster detail...","Enoki detail...","Portabello detail...","Porcini detail...","Morel detail..."};
+        icon = new int[]{R.drawable.agaricus, R.drawable.chantterelle, R.drawable.crimini, R.drawable.shiitake, R.drawable.oyster, R.drawable.enoki, R.drawable.portabello, R.drawable.porcini, R.drawable.morel};
+
+        listView = findViewById(R.id.listView);
+
+        for (int i =0; i<title.length; i++){
+            Model model = new Model(title[i], description[i], icon[i]);
+            //bind all strings in an array
+            arrayList.add(model);
+        }
+
+        //pass results to listViewAdapter class
+        adapter = new ListViewAdapter(this, arrayList);
+
+        //bind the adapter to the listview
+        listView.setAdapter(adapter);
+
     }
 
     @Override
@@ -47,6 +77,27 @@ public class Ediable extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.ediable, menu);
+
+        MenuItem myActionMenuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)myActionMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (TextUtils.isEmpty(s)){
+                    adapter.filter("");
+                    listView.clearTextFilter();
+                }
+                else {
+                    adapter.filter(s);
+                }
+                return true;
+            }
+        });
         return true;
     }
 
@@ -58,7 +109,9 @@ public class Ediable extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
+            return true;
+        } else if (id == R.id.action_contact){
             return true;
         }
 
@@ -71,13 +124,15 @@ public class Ediable extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.ediable) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.non_ediable) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.ediable_pre) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.home_made) {
+
+        } else if (id == R.id.facts) {
 
         } else if (id == R.id.nav_share) {
 
